b36ce9d78cfc4d48af18b1c39cfb83b3b57c080d
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-18 17:46:32 +0100

    feat: initialized development

---
+++ config/config.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import router from "../routes/router.js";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+export const PORT = process.env.PORT || 4000;
+
+export default app;
