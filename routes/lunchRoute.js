9f36ca1f62e2dd57d78572d1708dd3bfe363517a
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-19 22:50:40 +0100

    fix: fixed 187cfc1..d828f47 bug

--- routes/lunchRoute.js
+++ routes/lunchRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+// @IpieA
 
-router.post('/send', sendLunch);
-router.get('/:id', getSpecificLunch);
-router.get('/all', getAllLunch);
-router.put('/redeem/:id', getUserLunch);
+// router.post('/send', sendLunch);
+// router.get('/:id', getSpecificLunch);
+// router.get('/all', getAllLunch);
+// router.put('/redeem/:id', getUserLunch);
 
 export default router;
