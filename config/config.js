a5d0360240edf7c40ff07a1dc80535325e8900f3
Cyrus-11 <ayomideoluwayoyinsola@gmail.com>
2023-09-19 23:36:42 +0100

    fixed conflict

--- config/config.js
+++ config/config.js
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 import router from "../routes/router.js";
 import lunchRouter from "../routes/lunchRoute.js";
 import withdrawalRouter from "../routes/withdrawalRoute.js";
+import organizationRouter from "../routes/organizationRoutes.js";
+import authorizationRouterouter from "../routes/authorizationRoutes.js";
+import organizationRouter from "..routes/userRoutes.js";
 
 
 
@@ -17,7 +20,13 @@ app.use(router);
 // app.use(joi);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
+app.use('/api/lunch', lunchRouter);
+app.use('/api/withdrawal', withdrawalRouter);
+app.use('/api/organization', organizationRouter);
+app.use('/api/auth', authorizationRouter);
+app.use('/api/user', userRouter);
 
 export const PORT = process.env.PORT || 4000;
 
