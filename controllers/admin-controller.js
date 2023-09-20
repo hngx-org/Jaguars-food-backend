41216684241d44c5a94ca6933a28075c04301917
nireayoo <seminireayo@gmail.com>
2023-09-20 23:27:16 +0100

    added new function to admin controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,10 +2,17 @@ import asyncHandler from "express-async-handler";
 
 //Admin or organization
 
+
+const createAdmin = asyncHandler(async(req,res) =>
+{
+
+})
+
 const createInvite = asyncHandler(async(req,res) =>
 {
 
 })
+
 
 export{
     createInvite,
