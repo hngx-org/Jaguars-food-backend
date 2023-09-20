355ab2b9d8cc2a617af521fe76f496162cdf9cc6
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-09-20 14:52:46 +0100

    feat: Implement GET /api/users endpoint with dummy data
    Added the GET /api/users route to fetch all users. Dummy data is used for testing purposes until the SQL database is ready.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,47 +1,60 @@
 import asyncHandler from "express-async-handler";
 
+// when the databse is integrated, i'd refernece this model
+// import User from "./models";
+
+//Relying on dummy data to test endpoint
+const Users = [
+  {
+    name: "John Doe",
+    email: "john@mail.com",
+    profile_picture: "user-profile-picture-url",
+    user_id: "",
+  },
+  {
+    name: "John Doe",
+    email: "john@mail.com",
+    profile_picture: "user-profile-picture-url",
+    user_id: "",
+  },
+];
+
 //user is employee
 //GET USER PROFILE
-const getUserProfile = asyncHandler(async(req,res) =>
-{
-
-})
+const getUserProfile = asyncHandler(async (req, res) => {});
 
 //EDIT USER PROFILE
-const editUserProfile = asyncHandler(async(req,res) =>
-{
-
-})
+const editUserProfile = asyncHandler(async (req, res) => {});
 
 //ADD USER BANK ACCOUNT
-const addUserBank = asyncHandler(async(req,res) =>
-{
-
-})
+const addUserBank = asyncHandler(async (req, res) => {});
 
 //GET ALL USERS
-const getAllUsers = asyncHandler(async(req,res) =>
-{
-
-})
+const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    // const Users = await User.findAll() <-- this is a method on the sequalize model which is not yet available
+    res.json({
+      message: "successfully created bank account",
+      statusCode: 200,
+      data: Users,
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
 
 //GET(SEARCH) USER BY NAME OR MAIL
-const searchUser = asyncHandler(async(req,res) =>
-{
-
-})
+const searchUser = asyncHandler(async (req, res) => {});
 
 //CREATE WITHDRAWAL REQUEST
-const createWithdrawal= asyncHandler(async(req,res) =>
-{
+const createWithdrawal = asyncHandler(async (req, res) => {});
 
-})
-
-export{
-    getUserProfile,
-    editUserProfile,
-    getAllUsers,
-    addUserBank,
-    searchUser,
-    createWithdrawal,
-}
+export {
+  getUserProfile,
+  editUserProfile,
+  getAllUsers,
+  addUserBank,
+  searchUser,
+  createWithdrawal,
+};
