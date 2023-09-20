e592a2d6e20b49ce8baab7335fa6feb7f43de231
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-20 15:44:46 +0100

    feat:added mysql as config in dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,9 +1,12 @@
+import mysql2 from 'mysql2'
+
 export const dev_dbconfig = {
   HOST: process.env.MYSQL_ADDON_HOST || "127.0.0.1",
   USER: process.env.MYSQL_ADDON_USER || "root",
   PASSWORD: process.env.MYSQL_ADDON_PASSWORD || "",
   DB: process.env.MYSQL_ADDON_DB || "jaguar_food_app",
   dialect: "mysql",
+  dialectModule:mysql2,
 
   pool: {
     max: 5,
