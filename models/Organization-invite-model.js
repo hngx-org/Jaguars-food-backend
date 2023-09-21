304e718ebfe85242c8677d3024dcf12e533364c6
unknown <MhykelB>
2023-09-21 21:36:12 +0100

    changes

--- models/Organization-invite-model.js
+++ models/Organization-invite-model.js
@@ -1,17 +1,12 @@
-const OrganizationInvites = sequelize.define(
-    "organization_invites",
-    {
-     
-  
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isEmail: true,
-        },
-        unique: true,
-      }
+const OrganizationInvites = sequelize.define("organization_invites", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+    unique: true,
+  },
+});
 
-  });
-  
-  export { OrganizationInvites };
+export { OrganizationInvites };
