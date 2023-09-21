0a0b5081fc179b349d02264169e966e91e0034f0
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-09-21 11:49:08 +0100

    implemented redeeming lunches and initiating withdrawals

--- controllers/lunch-controller.js
+++ controllers/lunch-controller.js
@@ -1,7 +1,7 @@
-import asyncHandler from "express-async-handler";
-import { User } from "../models/user.model";
-import { Lunches } from "../models/lunches.model";
-import { Withdrawals } from "../models/withdrawals.model";
+import asyncHandler from 'express-async-handler';
+import { User } from '../models/user.model';
+import { Lunches } from '../models/lunches.model';
+import { Withdrawals } from '../models/withdrawals.model';
 
 //SEND A LUNCH
 const createLunch = asyncHandler(async (req, res) => {});
@@ -24,14 +24,14 @@ const redeemUserLunch = asyncHandler(async (req, res) => {
 
     if (!lunchID) {
       res.status(404);
-      throw new Error("Lunch not found");
+      throw new Error('Lunch not found');
     }
 
     // Fetch the current user
     const user = await User.findOne({ where: { id: id } });
     if (!user) {
       res.status(404);
-      throw new Error("User not found");
+      throw new Error('User not found');
     }
 
     const curBal = user.lunch_credit_balance;
@@ -39,26 +39,26 @@ const redeemUserLunch = asyncHandler(async (req, res) => {
     // if current  balance is less than the amount
     if (curBal < amount) {
       res.status(400);
-      throw new Error("Insufficient balance");
+      throw new Error('Insufficient balance');
     }
 
-    // update use balance
+    // update user balance
     await User.update({ lunch_credit_balance: curBal - amount }, { where: { id: id } });
 
     // create a withdrawal
     const withdrawal = await Withdrawals.create({
       id: lunchID,
       user_id: id,
-      status: "completed",
+      status: 'completed',
       amount: amount,
     });
 
     // update lunch status
     await Lunches.update({ redeemed: true }, { where: { id: lunch_id } });
 
-    res.status(200).json({ status: "success", message: "Lunch redeemed successfully", data: withdrawal });
+    res.status(200).json({ status: 'success', message: 'Lunch redeemed successfully', data: withdrawal });
   } catch (err) {
-    res.status(500).json({ status: "error", message: "Something went wrong", data: err });
+    res.status(500).json({ status: 'error', message: 'Something went wrong', data: err });
   }
 });
 
