199d9d1388834598e468e73d1df55e2a20a65de9
Ojo Boluwatife Fiyinfoluwa <110172783+boluwatife010@users.noreply.github.com>
2023-09-21 18:18:32 +0100

    Update docs.js

--- docs.js
+++ docs.js
@@ -19,15 +19,147 @@ const options = {
     },
     servers: [
       {
-        url: `"http://localhost:+${PORT}`,
+        url: `http://localhost:${PORT}`,
       },
       {
-        url: `https://jaguars-food-backend.vercel.app/`,
+        url: "https://jaguars-food-backend.vercel.app/",
       },
     ],
   },
+  components: {
+    securitySchemes: {
+      BasicAuth: {
+        type: "http",
+        scheme: "basic",
+        description: "Basic Authentication",
+      },
+      BearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        description: "Bearer Token Authentication",
+      },
+    },
+  },
   apis: ["./routes/*.js"],
+  paths: {
+    "/api/auth/login": {
+      post: {
+        summary: "Login",
+        description: "Login to the application with email and password.",
+        requestBody: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  email: {
+                    type: "string",
+                    format: "email",
+                    example: "user@example.com",
+                  },
+                  password: {
+                    type: "string",
+                    example: "password123",
+                  },
+                },
+                required: ["email", "password"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "Successful login",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    message: {
+                      type: "string",
+                      example: "User authenticated successfully",
+                    },
+                    statusCode: {
+                      type: "integer",
+                      example: 200,
+                    },
+                    data: {
+                      type: "object",
+                      properties: {
+                        access_token: {
+                          type: "string",
+                          example: "your-auth-token-here",
+                        },
+                        email: {
+                          type: "string",
+                          example: "email@mail.com",
+                        },
+                        id: {
+                          type: "string",
+                          example: "random_id",
+                        },
+                        isAdmin: {
+                          type: "boolean",
+                          example: true,
+                        },
+                      },
+                    },
+                  },
+                  required: ["message", "statusCode", "data"],
+                },
+                example: {
+                  message: "User authenticated successfully",
+                  statusCode: 200,
+                  data: {
+                    access_token: "your-auth-token-here",
+                    email: "email@mail.com",
+                    id: "random_id",
+                    isAdmin: true,
+                  },
+                },
+              },
+            },
+          },
+          400: {
+            description: "Bad Request",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    message: {
+                      type: "string",
+                      example: "Bad Request: Invalid input data",
+                    },
+                    statusCode: {
+                      type: "integer",
+                      example: 400,
+                    },
+                  },
+                },
+                example: {
+                  message: "Bad Request: Invalid input data",
+                  statusCode: 400,
+                },
+              },
+            },
+          },
+        },
+        security: [
+          {
+            BasicAuth: [], 
+          },
+          {
+            BearerAuth: [],
+          },
+        ],
+      },
+    },
+    // Add more API paths here
+  },
 };
+
 const specs = SwaggerJsDoc(options);
 
 export const docRouter = express.Router();
