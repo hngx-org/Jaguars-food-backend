6ce1d17ebbd2302c654eb6248e58be8918f4e119
nireayoo <seminireayo@gmail.com>
2023-09-21 11:09:26 +0100

    corrected auth folder

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,5 +1,47 @@
-import userSignUp from './userSignUpController'
+import asyncHandler from "express-async-handler";
+import bcrypt from 'bcrypt'
 
 // Import all the auth controllers in one file - userSignUp, login, 
 
-export  {userSignUp}
+const userSignUp = asyncHandler(async(req,res) =>
+{
+
+})
+
+const adminSignUp =  asyncHandler(async(req,res) =>
+{
+
+})
+
+const userLogin =  asyncHandler(async(req,res) =>
+{
+
+})
+
+
+// Function to generate a hashed password
+async function generateHashedPassword(userPassword, saltRounds) {
+  try {
+    const hashedPassword = await bcrypt.hash(userPassword, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// async function isMatch(userPassword, hashedPassword) {
+//   try {
+//     const match = await bcrypt.compare(userPassword, hashedPassword);
+//     return match;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+export {
+  adminSignUp,
+  userSignUp,
+  userLogin,
+  generateHashedPassword,
+
+}
