5ea47643edd7abb4d855cf315749739d128d60d8
favour-22 <105045421+favour-22@users.noreply.github.com>
2023-09-21 16:14:58 +0100

    CHORE:Moved organizationinvite to auth folder

--- routes/organzatIoninvite.js
+++ routes/organzatIoninvite.js
@@ -1,30 +1,24 @@
 import express from "express";
 import crypto from "crypto";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+
+//importing isAdmin to check if organization is an  Admin 
+import isAdmin from "../middlewares/isAdmin"
+import {getToken ,verifyToken} from "../utils/tokens"
+
 
 const router = express.Router();
 dotenv.config();
 
-
-// Authentication middleware to ensure only admin users can access the endpoint
-function adminAuthMiddleware(req, res, next) {
-  // Implement admin authentication logic here, e.g., check if the user is admin 
-  // If the user is not an admin, return an error response
-  // If the user is an admin, call the next middleware
-  //proceed to the invitation logic
-}
-
 // API endpoint for sending an invitation
-router.post(`/organizations/invite`, adminAuthMiddleware, (req, res) => {
+router.post(`/organizations/invite`, isAdmin, (req, res) => {
   // Parse request body and extract necessary details
   const { email } = req.body;
 
   const secretKey = process.env.SECRET_KEY
   // Generate a unique invitation token
   const invitationToken = generateInvitationToken(email, secretKey);
-
-  // Saving invitation details to a database 
-  saveInvitationToDatabase(email, invitationToken);
 
   // Sending invitation email
   sendInvitationEmail(email, invitationToken);
@@ -35,21 +29,19 @@ router.post(`/organizations/invite`, adminAuthMiddleware, (req, res) => {
 
 
 // Helper function to generate a unique invitation token
-function generateInvitationToken(email, secretKey) {
-  // Implement logic to generate a unique invitation token
-  // This token can be used to identify and verify the invitation when accepting it
+//function generateInvitationToken(email, secretKey) {
 
-  const dateStamp = Date.now();
-  const hashedData = `${email}${dateStamp}`;
-  const token = crypto.createHmac('sha256', secretKey).update(hashedData).digest('hex');
-  return token;
-}
+//  const dateStamp = Date.now();
+//  const hashedData = `${email}${dateStamp}`;
+//  const token = crypto.createHmac('sha256', secretKey).update(hashedData).digest('hex');
+//  return token;
+//}
 // const secretKey = crypto.randomBytes(32).toString('hex');// generate secret key. Save secret key in .env file and delete this line of code. 
 
 // Helper function to save invitation details to a database
-function saveInvitationToDatabase(email, invitationToken) {
+//function saveInvitationToDatabase(email, invitationToken) {
   // Implement  logic to save the invitation details to a database
-}
+//}
 
 // Helper function to send the invitation email
 
@@ -57,6 +49,28 @@ function sendInvitationEmail(email, invitationToken) {
   // Implement y logic to send the invitation email using a library like Nodemailer or 
   //third-party service
 }
+
+function generateInvitationToken(email, secretKey){
+  // Assuming you have required dependencies like `jsonwebtoken` and set the JWT_SECRET variable
+const userData = {
+  email: email,
+};
+
+getToken(userData)
+  .then((token) => {
+    //console.log("Generated token:", token);
+    // Use the token as needed
+  })
+  .catch((error) => {
+    //console.error("Error generating token:", error);
+  });
+  
+  return getToken(userData);
+};
+
+
+
+
 
 
 export default router;
