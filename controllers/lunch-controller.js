381079f795a9424c22241678197228093f236c77
Emmanuel Olagoke <emma.goke05@gmail.com>
2023-09-21 13:57:38 +0100

    Added the getLunch Handler

--- controllers/lunch-controller.js
+++ controllers/lunch-controller.js
@@ -1,29 +1,37 @@
-import asyncHandler from "express-async-handler";
-
+import asyncHandler from 'express-async-handler';
+import Lunch from '../models/lunches.model';
 
 //SEND A LUNCH
-const createLunch = asyncHandler(async(req,res) =>
-{
-
-})
+const createLunch = asyncHandler(async (req, res) => {});
 
 //GET A LUNCH
-const getLunch = asyncHandler(async(req,res) =>
-{
+const getLunch = asyncHandler(async (req, res) => {
+  const { id: lunchId } = req.params;
 
-})
+  if (lunchId) {
+    const lunch = await Lunch.findOne({ where: { id: lunchId } });
+    if (!lunch) {
+      res.status(404);
+      throw new Error(`No Lunch exit for ${id}`);
+    } else {
+      const data = {
+        receiverId: lunch.receiverId,
+        senderId: lunch.senderId,
+        quantity: lunch.quantity,
+        redeemed: lunch.redeemed,
+        note: lunch.note,
+        created_at: lunch.created_at,
+        id: lunch.id,
+      };
+      const msg = 'The Lunch is available';
+      res.status(200).json({ message: msg, statusCode: 200, data });
+    }
+    res.status(400);
+    throw new Error('Please provide an id');
+  }
+});
 
 //GET ALL LUNCHES
-const getAllLunches = asyncHandler(async(req,res) =>
-{
+const getAllLunches = asyncHandler(async (req, res) => {});
 
-})
-
-
-
-export{
-    createLunch,
-    getLunch,
-    getAllLunches,
-
-}
+export { createLunch, getLunch, getAllLunches };
