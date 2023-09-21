10b52807c1831986477b4cef7a4c4399a2f67678
boluwatife010 <temitopeboluojo@gmail.com>
2023-09-21 10:29:20 +0100

    updated the create a lunch and the withdrawals

--- controllers/lunch-controller.js
+++ controllers/lunch-controller.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import { Lunches } from "../models/lunches.model";
 
 
 //SEND A LUNCH
@@ -10,7 +11,17 @@ const createLunch = asyncHandler(async(req,res) =>
 //GET A LUNCH
 const getLunch = asyncHandler(async(req,res) =>
 {
-
+    try {
+        const lunchId = req.params.lunchId;
+        const lunch = await Lunches.findByPk(lunchId);
+        if (!lunch) {
+          return res.status(404).json({ error: 'Lunch not found' });
+        }
+        res.status(200).json({ lunch });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
 })
 
 //GET ALL LUNCHES
