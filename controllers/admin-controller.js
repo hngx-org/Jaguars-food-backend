9ccc3f529eca8a0e448cbf6f54481122755d1460
Praise Izuagie <izuagiepraise@gmail.com>
2023-09-21 23:13:04 +0100

    Add createAdmin logic

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,21 +1,61 @@
 import asyncHandler from "express-async-handler";
 
+import { organization as Organization } from "../models/organization.model";
+import { verifyToken } from "../utils/tokens";
+
 //Admin or organization
 
+const createAdmin = asyncHandler(async (req, res) => {
+  const { organization_name, lunch_price, currency_code } = req.body;
 
-const createAdmin = asyncHandler(async(req,res) =>
-{
+  const authHeader = req.headers.authorization;
+  if (!(authHeader && authHeader.startsWith("Bearer "))) {
+    return res.status(401).json({
+      message: "An error occured",
+      errorMessage: "Invalid auth header",
+    });
+  }
+  const token = authHeader.split(" ")[1];
 
-})
+  try {
+    verifyToken(token);
+  } catch (error) {
+    return res.status(403).json({
+      message: "An error occured",
+      errorMessage: "Not authorized",
+    });
+  }
 
-const createInvite = asyncHandler(async(req,res) =>
-{
+  await Organization.findOrCreate({
+    where: {
+      organization_name: organization_name,
+    },
+    defaults: {
+      currency_code: currency_code ?? "NGN",
+      lunch_price: lunch_price ?? 1000,
+    },
+  })
+    .then(([user, created]) => {
+      if (!created) {
+        return res.status(500).send({
+          message: "An Error Occured",
+          errorMessage: `${organization_name} already exists`,
+        });
+      }
 
-})
+      return res.status(201).send({
+        message: "Organization Created Successfully",
+        data: user.dataValues,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).send({
+        message: "An Error Occured",
+        errorMessage: error.message,
+      });
+    });
+});
 
+const createInvite = asyncHandler(async (req, res) => {});
 
-export{
-    createInvite,
-    createAdmin,
-
-}
+export { createInvite, createAdmin };
