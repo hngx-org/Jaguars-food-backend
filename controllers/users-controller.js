2b3deadda290f5d5c980829e7f8f51cdb337c53d
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-21 01:28:02 +0100

    feat: Add new routes and controllers in userRoute.js
    In this commit, I added new routes and associated controller functions to the userRoute.js file to handle various user-related actions:
    - Added a route to edit user profiles with the corresponding controller function.
    - Added a route to create withdrawal requests with the associated controller function.
    
    These changes extend the functionality of the user routes in the application.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,47 +1,116 @@
-import asyncHandler from "express-async-handler";
+import asyncHandler from 'express-async-handler';
 
 //user is employee
 //GET USER PROFILE
-const getUserProfile = asyncHandler(async(req,res) =>
-{
-
-})
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 //EDIT USER PROFILE
-const editUserProfile = asyncHandler(async(req,res) =>
-{
-
-})
+const editUserProfile = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 //ADD USER BANK ACCOUNT
-const addUserBank = asyncHandler(async(req,res) =>
-{
-
-})
+const addUserBank = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (user) {
+    user.bank = {
+      bankName: req.body.bankName,
+      accountNumber: req.body.accountNumber,
+      accountName: req.body.accountName,
+    };
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      bank: updatedUser.bank,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
 //GET ALL USERS
-const getAllUsers = asyncHandler(async(req,res) =>
-{
-
-})
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
 
 //GET(SEARCH) USER BY NAME OR MAIL
-const searchUser = asyncHandler(async(req,res) =>
-{
-
-})
+const searchUser = asyncHandler(async (req, res) => {
+  const nameOrEmail = req.params.nameoremail;
+  const users = await User.find({
+    $or: [
+      { name: { $regex: nameOrEmail, $options: 'i' } },
+      { email: { $regex: nameOrEmail, $options: 'i' } },
+    ],
+  });
+  res.json(users);
+});
 
 //CREATE WITHDRAWAL REQUEST
-const createWithdrawal= asyncHandler(async(req,res) =>
-{
+const createWithdrawal = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (user) {
+    const withdrawal = {
+      amount: req.body.amount,
+      bankName: req.body.bankName,
+      accountNumber: req.body.accountNumber,
+      accountName: req.body.accountName,
+    };
+    user.withdrawals.push(withdrawal);
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      bank: updatedUser.bank,
+      withdrawals: updatedUser.withdrawals,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
 
-})
-
-export{
-    getUserProfile,
-    editUserProfile,
-    getAllUsers,
-    addUserBank,
-    searchUser,
-    createWithdrawal,
-}
+export {
+  getUserProfile,
+  editUserProfile,
+  getAllUsers,
+  addUserBank,
+  searchUser,
+  createWithdrawal,
+};
