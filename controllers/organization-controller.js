b1bf625e895f9c2d9cfa6349c00e0c1b2288384d
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-21 15:49:07 +0100

    fix: Made use of error handler to handle errors

--- controllers/organization-controller.js
+++ controllers/organization-controller.js
@@ -1,5 +1,4 @@
 import asyncHandler from "express-async-handler";
-import jwt from "jsonwebtoken";
 import { organization } from "../models/organization.model.js";
 import errHandler from "../middlewares/errHandler.js";
 
@@ -10,34 +9,31 @@ const createOrganization = asyncHandler(async (req, res) => {
   organization.findOne({
     where: { name },
   }).then(async (user) => {
-    if (user) return res.status(504).send({
-      message: "An Error Occured",
-      errorMessage: `${name} exists try using another name`,
-    });
+    if (user) {
+      res.status(400);
+      throw new Error(`${name} Exists, please use another name`);
+    } else {
+      await organization
+        .create({
+          name: name,
+          currency: currency ?? "NGN",
+          lunch_price: lunch_price ?? 1000,
+        })
+        .then((createdUser) => {
+          return res.status(202).send({
+            message: "Organization Created Successfully",
+            data: createdUser.dataValues,
+          });
+        })
+        .catch((error) => {
+          res.status(504);
+          throw new Error(error.errorMessage);
+        });
+    }
 
-    await organization
-      .create({
-        name: name,
-        currency: currency ?? "NGN",
-        lunch_price: lunch_price ?? 1000,
-      })
-      .then((createdUser) => {
-        return res.status(202).send({
-          message: "Organization Created Successfully",
-          data: createdUser.dataValues,
-        });
-      })
-      .catch((error) => {
-        return res.status(504).send({
-          message: "An Error Occured",
-          errorMessage: error.message,
-        });
-      });
+
   }).catch((error) => {
-    return res.status(500).send({
-      message: "An Error Occured",
-      errorMessage: error.message,
-    });
+    errHandler(error, req, res);
   });
 
 
