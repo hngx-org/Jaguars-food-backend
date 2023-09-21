fa429245704a375f59137afc29e83c86d3615616
nireayoo <seminireayo@gmail.com>
2023-09-21 09:33:17 +0100

    renamed admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createAdmin, createInvite } from "../controllers/admin-controller";
 const router = Router();
 
-router.post('/admin/new', createAdmin);
-router.post('/admin/invite', createInvite);
+router.post('/organization/create', createAdmin);
+router.post('/organization/invite', createInvite);
 
 export default router;
