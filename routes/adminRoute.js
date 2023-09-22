4fa7ebaceea77aa278115f2822a58e9f0761e4c5
Praise Izuagie <izuagiepraise@gmail.com>
2023-09-22 09:27:10 +0100

    Update "create" method to PUT

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 import { createAdmin, createInvite } from "../controllers/admin-controller";
 
-router.post("/create", createAdmin);
+router.put("/create", createAdmin);
 router.post("/invite", createInvite);
 
 export default router;
