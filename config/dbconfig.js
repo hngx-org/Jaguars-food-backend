e84893ec05653402ac49f08b46e9c9615a342a49
MhykelB <MhykelB>
2023-09-22 03:21:28 +0100

    revert chnages made to database config

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,10 +3,10 @@ import { configDotenv } from "dotenv";
 export const dev_dbconfig = {
   HOST: process.env.MYSQL_ADDON_HOST || "127.0.0.1",
   USER: process.env.MYSQL_ADDON_USER || "root",
-  PASSWORD: process.env.MYSQL_ADDON_PASSWORD || "mhykel666",
+  PASSWORD: process.env.MYSQL_ADDON_PASSWORD || "",
   DB: process.env.MYSQL_ADDON_DB || "jaguar_food_app",
-
   dialect: "mysql",
+
   pool: {
     max: 5,
     min: 0,
