f9c3ed849a7f718673a900abc8871480035880e4
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-22 11:38:10 +0100

    feat:worked on route

--- routes/withdrawalRoute.js
+++ routes/withdrawalRoute.js
@@ -1,11 +1,11 @@
-import {Router} from "express";
+import { Router } from "express";
 import { createWithdrawal } from "../controllers/users-controller.js";
 import { verifyAccessToken } from "../utils/tokens.js";
 
 const router = Router();
 
 // Withdrawal Request
-router.post('/request',verifyAccessToken, createWithdrawal)
 
+router.post("/request", verifyAccessToken, createWithdrawal);
 
 export default router;
