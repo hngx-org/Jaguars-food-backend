d1f46d35ba7c1c4eafb84c6696ab0e546cc1eb31
dyCodes <yusufdaudu51@gmail.com>
2023-09-22 01:23:48 +0100

    fix es modules error

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,21 @@
-import asyncHandler from "express-async-handler";
-import { verifyToken } from "../utils/tokens";
+import asyncHandler from 'express-async-handler';
+import { verifyToken } from '../utils/tokens.js';
 
 const authMiddleware = asyncHandler(async (req, res, next) => {
-  try {
-    if (req.headers.authorization.startsWith("Bearer")) {
-      const token = req.headers.authorization.split(" ")[1];
+    try {
+        if (req.headers.authorization.startsWith('Bearer')) {
+            const token = req.headers.authorization.split(' ')[1];
 
-      if (!token) throw new Error("Unauthorized");
+            if (!token) throw new Error('Unauthorized');
 
-      const data = await verifyToken(token);
-      req.user = data;
-      next();
-    } else {
-      throw new Error("Unauthorized! no token in header");
+            const data = await verifyToken(token);
+            req.user = data;
+            next();
+        } else {
+            throw new Error('Unauthorized! no token in header');
+        }
+    } catch (error) {
+        throw new Error(error);
     }
-  } catch (error) {
-    throw new Error(error);
-  }
 });
 export default authMiddleware;
