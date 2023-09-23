26d08695c21ccc94d88d43b45337dcbe3559f80a
MhykelB <104434185+MhykelB@users.noreply.github.com>
2023-09-23 17:15:18 +0100

    Mhyke get org wallet balance (#86)
    * env
    
    * pulled develop
    
    * no changes yet
    
    * pulled again
    
    * get org wallet balance
    
    ---------
    
    Co-authored-by: unknown <MhykelB>

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const router = require('../routes/router.js');
-const lunchRouter = require('../routes/lunchRoute.js');
-const withdrawalRouter = require('../routes/withdrawalRoute.js');
-const { authRouter, orgRouter } = require('../routes/authenticationRoute.js');
-const userRoute = require('../routes/userRoute.js');
-const errHandler = require('../middlewares/errHandler.js');
-const notFound = require('../middlewares/notFound.js');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const router = require("../routes/router.js");
+const lunchRouter = require("../routes/lunchRoute.js");
+const withdrawalRouter = require("../routes/withdrawalRoute.js");
+const { authRouter, orgRouter } = require("../routes/authenticationRoute.js");
+const userRoute = require("../routes/userRoute.js");
+const errHandler = require("../middlewares/errHandler.js");
+const notFound = require("../middlewares/notFound.js");
 
 const app = express();
 dotenv.config();
@@ -17,14 +17,14 @@ app.use(cors());
 app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // ROUTES
-app.use('/api/lunch', lunchRouter);
-app.use('/api/withdrawal', withdrawalRouter);
-app.use('/api', userRoute);
-app.use('/api/auth', authRouter);
-app.use('/api/orgs', orgRouter);
+app.use("/api/lunch", lunchRouter);
+app.use("/api/withdrawal", withdrawalRouter);
+app.use("/api", userRoute);
+app.use("/api/auth", authRouter);
+app.use("/api/orgs", orgRouter);
 
 // MIDDLEWARES
 app.use(notFound);
--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -1,44 +1,46 @@
-const express = require('express');
+const express = require("express");
 const {
-	createAdmin,
-	createInvite,
-	searchOrg,
-	update0rgFoodPrice,
-	update0rgWalletBalance,
-} = require('../controllers/auth/adminOrgController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const isAdmin = require('../middlewares/isAdmin');
+  createAdmin,
+  createInvite,
+  searchOrg,
+  update0rgFoodPrice,
+  update0rgWalletBalance,
+  orgWalletBalance,
+} = require("../controllers/auth/adminOrgController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const isAdmin = require("../middlewares/isAdmin");
 const {
-	Login,
-	staffSignUp,
-} = require('../controllers/auth/userAuthController');
+  Login,
+  staffSignUp,
+} = require("../controllers/auth/userAuthController");
 
 const authRouter = express.Router();
 const orgRouter = express.Router();
 // const orgRouter = express.Router();
 
 //ADMIN SIGN UP ROUTE
-authRouter.post('/user/signup', createAdmin);
-authRouter.get('/organization/:name', searchOrg);
-authRouter.post('/staff/signup', staffSignUp);
-authRouter.post('/login', Login);
+authRouter.post("/user/signup", createAdmin);
+authRouter.get("/organization/:name", searchOrg);
+authRouter.post("/staff/signup", staffSignUp);
+authRouter.post("/login", Login);
 
 // ORG ACTIVITIES
-orgRouter.post('/invite', authMiddleware, isAdmin, createInvite);
+orgRouter.post("/invite", authMiddleware, isAdmin, createInvite);
 orgRouter.patch(
-	'/wallet/update',
-	authMiddleware,
-	isAdmin,
-	update0rgWalletBalance
+  "/wallet/update",
+  authMiddleware,
+  isAdmin,
+  update0rgWalletBalance
 );
-orgRouter.patch('/lunch/update', authMiddleware, isAdmin, update0rgFoodPrice);
+
+orgRouter.patch("/lunch/update", authMiddleware, isAdmin, update0rgFoodPrice);
 
 // TODO: Get org account balance
-// orgRouter.get(
-// 	'organization/wallet',
-// 	authMiddleware,
-// 	isAdmin,
-// 	0rgWalletBalance
-// );
+orgRouter.get(
+  "/organization/wallet",
+  authMiddleware,
+  isAdmin,
+  orgWalletBalance
+);
 
 module.exports = { orgRouter, authRouter };
