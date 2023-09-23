d10535ec7b2fd2da1bb05b5843087bdeb34fd9a7
MhykelB <MhykelB>
2023-09-23 15:49:41 +0100

    pulled again

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const {
-	createAdmin,
-	createInvite,
-	searchOrg,
-} = require('../controllers/auth/adminOrgController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const isAdmin = require('../middlewares/isAdmin');
+  createAdmin,
+  createInvite,
+  searchOrg,
+} = require("../controllers/auth/adminOrgController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const isAdmin = require("../middlewares/isAdmin");
 const {
-	Login,
-	staffSignUp,
-} = require('../controllers/auth/userAuthController');
+  Login,
+  staffSignUp,
+} = require("../controllers/auth/userAuthController");
 
 const router = express.Router();
 
 //ADMIN SIGN UP ROUTE
-router.post('/user/signup', createAdmin);
-router.post('/organization/invite', authMiddleware, isAdmin, createInvite);
-router.get('/organization/:name', searchOrg);
-router.post('/organization/staff/signup', staffSignUp);
+router.post("/user/signup", createAdmin);
+router.post("/organization/invite", authMiddleware, isAdmin, createInvite);
+router.get("/organization/:name", searchOrg);
+router.post("/organization/staff/signup", staffSignUp);
 // router.post('/organization/staff/signup', authMiddleware, createInvite);
-router.post('/login', Login);
+router.post("/login", Login);
 
 // ADMIN LOGIN ROUTE
 // router.route('/login').post(logInAdmin);
