bf74d6bb82d30597e6330d7df5bf59dc21dd0904
joshua-jj <akhidenorjoshua@gmail.com>
2023-09-23 20:40:50 +0100

    Include add bank account functionality

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -4,61 +4,61 @@ const { Op } = require('sequelize');
 
 //GET USER PROFILE
 const getUserProfile = asyncHandler(async (req, res) => {
-	const id = req.user.id;
-	const user = await db.user.findOne({ where: { id } });
-	if (user) {
-		res.json(user);
-	} else {
-		res.status(404);
-		throw new Error('User not found');
-	}
+  const id = req.user.id;
+  const user = await db.user.findOne({ where: { id } });
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // TODO:: Check ctrls to be sure db was queried
 //EDIT USER PROFILE
 const editUserProfile = asyncHandler(async (req, res) => {
-	const user = req.user;
-	if (user) {
-		user.name = req.body.name || user.name;
-		user.email = req.body.email || user.email;
-		if (req.body.password) {
-			user.password = req.body.password;
-		}
-		const updatedUser = await db.user.save();
-		res.json({
-			_id: updatedUser._id,
-			name: updatedUser.name,
-			email: updatedUser.email,
-			isAdmin: updatedUser.isAdmin,
-		});
-	} else {
-		res.status(404);
-		throw new Error('User not found');
-	}
+  const user = req.user;
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await db.user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // TODO
 //ADD USER BANK ACCOUNT
 const addUserBank = asyncHandler(async (req, res) => {
-	const user = req.user;
-	if (user) {
-		user.bank = {
-			bankName: req.body.bankName,
-			accountNumber: req.body.accountNumber,
-			accountName: req.body.accountName,
-		};
-		const updatedUser = await db.user.save();
-		res.json({
-			_id: updatedUser._id,
-			name: updatedUser.name,
-			email: updatedUser.email,
-			isAdmin: updatedUser.isAdmin,
-			bank: updatedUser.bank,
-		});
-	} else {
-		res.status(404);
-		throw new Error('User not found');
-	}
+  console.log(req.user);
+  const { bank_number, bank_code, bank_name } = req.body;
+  await db.user.update(
+    {
+      bankNumber: bank_number,
+      bankCode: bank_code,
+      bankName: bank_name,
+    },
+    {
+      where: {
+        id: req.user.id,
+      },
+    }
+  );
+
+	res.status(200).json({
+		statusCode: 200,
+		message: "Bank details successfully updated"
+	});
 });
 
 //GET ALL USERS
@@ -69,89 +69,85 @@ const addUserBank = asyncHandler(async (req, res) => {
  * @description Retrieve a list of all User or filter by 'name' query parameter.
  */
 const getAllUsers = asyncHandler(async (req, res) => {
-	try {
-		// Assuming you retrieve all users from the database
-		const users = await db.user.findAll({});
-		// Response data
-		const responseData = {
-			message: 'Successfully gotten all users',
-			statusCode: 200,
-			data: users.map((user) => ({
-				firstName: user.firstName,
-				lastName: user.lastName,
-				email: user.email,
-				profilePicture:
-					user.profile_picture || 'user-profile-picture-url', // Replace with the actual profile picture URL or a default value
-				id: user.id,
-				orgId: user.orgId,
-			})),
-		};
-		res.status(200).json(responseData);
-	} catch (error) {
-		// Handle errors and send an error response
-		console.error(error);
-		throw new Error(error);
-	}
+  try {
+    // Assuming you retrieve all users from the database
+    const users = await db.user.findAll({});
+    // Response data
+    const responseData = {
+      message: 'Successfully gotten all users',
+      statusCode: 200,
+      data: users.map(user => ({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        profilePicture: user.profile_picture || 'user-profile-picture-url', // Replace with the actual profile picture URL or a default value
+        id: user.id,
+        orgId: user.orgId,
+      })),
+    };
+    res.status(200).json(responseData);
+  } catch (error) {
+    // Handle errors and send an error response
+    console.error(error);
+    throw new Error(error);
+  }
 });
 
 //GET(SEARCH) USER BY NAME OR MAIL
 const searchUser = asyncHandler(async (req, res) => {
-	const nameOrEmail = req.params.nameoremail;
-	// console.log(nameOrEmail);
-	const users = await db.user.findAll({
-		where: {
-			[Op.or]: [
-				{ firstName: { [Op.like]: '%' + nameOrEmail + '%' } },
-				{ lastName: { [Op.like]: '%' + nameOrEmail + '%' } },
-				{ email: { [Op.like]: '%' + nameOrEmail + '%' } },
-			],
-		},
-	});
-	res.json(users);
+  const nameOrEmail = req.params.nameoremail;
+  // console.log(nameOrEmail);
+  const users = await db.user.findAll({
+    where: {
+      [Op.or]: [
+        { firstName: { [Op.like]: '%' + nameOrEmail + '%' } },
+        { lastName: { [Op.like]: '%' + nameOrEmail + '%' } },
+        { email: { [Op.like]: '%' + nameOrEmail + '%' } },
+      ],
+    },
+  });
+  res.json(users);
 });
 
 // TODO
 //CREATE WITHDRAWAL REQUEST
 const createWithdrawal = asyncHandler(async (req, res) => {
-	const user = req.user;
+  const user = req.user;
 
-	if (user) {
-		const withdrawal = {
-			amount: req.body.amount,
-			bankName: req.body.bankName,
-			accountNumber: req.body.accountNumber,
-			accountName: req.body.accountName,
-		};
-		const lunchId = req.body.lunchId;
-		if (lunchId) {
-			// Update the status of the lunch from 'redeemed: false' to 'redeemed: true'
-			await Lunches.update(
-				{ redeemed: true },
-				{ where: { id: lunchId, redeemed: false } }
-			);
-		}
-		user.withdrawals.push(withdrawal);
-		const updatedUser = await db.user.save();
-		res.json({
-			_id: updatedUser._id,
-			name: updatedUser.name,
-			email: updatedUser.email,
-			isAdmin: updatedUser.isAdmin,
-			bank: updatedUser.bank,
-			withdrawals: updatedUser.withdrawals,
-		});
-	} else {
-		// If user is not found (unauthenticated), return a 404 response with an error message
-		res.status(404);
-		throw new Error('User not found');
-	}
+  if (user) {
+    const withdrawal = {
+      amount: req.body.amount,
+      bankName: req.body.bankName,
+      accountNumber: req.body.accountNumber,
+      accountName: req.body.accountName,
+    };
+    const lunchId = req.body.lunchId;
+    if (lunchId) {
+      // Update the status of the lunch from 'redeemed: false' to 'redeemed: true'
+      await Lunches.update({ redeemed: true }, { where: { id: lunchId, redeemed: false } });
+    }
+    user.withdrawals.push(withdrawal);
+    const updatedUser = await db.user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      bank: updatedUser.bank,
+      withdrawals: updatedUser.withdrawals,
+    });
+  } else {
+    // If user is not found (unauthenticated), return a 404 response with an error message
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 module.exports = {
-	getUserProfile,
-	editUserProfile,
-	getAllUsers,
-	addUserBank,
-	searchUser,
-	createWithdrawal,
+  getUserProfile,
+  editUserProfile,
+  getAllUsers,
+  addUserBank,
+  searchUser,
+  createWithdrawal,
 };
