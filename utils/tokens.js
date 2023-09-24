e72d5e2fadfdaf3273930e14e9410646f018e5f5
Cyrus-11 <ayomideoluwayoyinsola@gmail.com>
2023-09-24 13:14:03 +0100

    resolved merge conflict

--- utils/tokens.js
+++ utils/tokens.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import jwt from 'jsonwebtoken';
-
-let JWT_SECRET = process.env.JWT_SECRET || 'JAGUARJAGUARJAGUAR';
-
-// GetToken
-export const getToken = (otp) => {
-    return new Promise((accept, reject) => {
-        jwt.sign({ otp } , JWT_SECRET, { expiresIn: '1d' }, (err, token) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                accept(token);
-            }
-        });
-    });
-};
-
-// Verify token
-// export const verifyToken = (token) => {
-//     return new Promise((accept, reject) => {
-//         jwt.verify(token, JWT_SECRET, (err, decoded) => {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 // console.log("Data", decoded.email);
-//                 accept(decoded);
-//             }
-//         });
-//     });
-// };
-
-
-
-export const verifyToken = (token) => {
-    const decodedToken = jwt.verify(token, JWT_SECRET)
-    if(decodedToken){
-        return(decodedToken)
-    }else{
-        return
-    } 
-    };
-=======
 const jwt = require('jsonwebtoken');
 
 let JWT_SECRET = process.env.JWT_SECRET || 'JAGUARJAGUARJAGUAR';
@@ -75,4 +31,3 @@ const verifyToken = (token) => {
 };
 
 module.exports = { getToken, verifyToken };
->>>>>>> develop
